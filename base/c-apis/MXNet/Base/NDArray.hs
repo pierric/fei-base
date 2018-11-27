046f5d105b47a17247889d76e08f847497b8f632
Jiasen Wu <jiasenwu@hotmail.com>
2018-11-27 15:57:19 +0100

    minor

--- base/c-apis/MXNet/Base/NDArray.hs
+++ base/c-apis/MXNet/Base/NDArray.hs
@@ -6,21 +6,20 @@ import Data.Vector.Storable (Vector)
 import qualified Data.Vector.Storable as V
 import qualified Data.Vector.Storable.Mutable as VMut
 import qualified MXNet.Base.Raw as I
-import MXNet.Base.Types (Device(..), DType)
+import MXNet.Base.Types (Context(..), contextCPU, DType)
 
 newtype NDArray a = NDArray { unNDArray :: I.NDArrayHandle}
 
-makeEmptyNDArray :: DType a => [Int] -> Device -> IO (NDArray a)
+makeEmptyNDArray :: DType a => [Int] -> Context -> IO (NDArray a)
 makeEmptyNDArray shape ctx = do
     array <- I.mxNDArrayCreate shape (_device_type ctx) (_device_id ctx) False
     return $ NDArray array
 
-makeNDArray :: DType a => [Int] -> Device -> Vector a -> IO (NDArray a)
+makeNDArray :: DType a => [Int] -> Context -> Vector a -> IO (NDArray a)
 makeNDArray shape ctx vec = do
     array <- I.mxNDArrayCreate shape (_device_type ctx) (_device_id ctx) False
     V.unsafeWith vec $ \p -> do
-        let size = V.length vec * sizeOf (V.head vec)
-        I.mxNDArraySyncCopyFromCPU array (castPtr p) size
+        I.mxNDArraySyncCopyFromCPU array (castPtr p) (V.length vec)
         return $ NDArray array
 
 ndshape :: NDArray a -> IO [Int]
@@ -30,7 +29,7 @@ ndsize :: NDArray a -> IO Int
 ndsize arr = product <$> ndshape arr
 
 full :: DType a => a -> [Int] -> IO (NDArray a)
-full value shape = makeNDArray shape (Device 1 0) $ V.replicate (product shape) value
+full value shape = makeNDArray shape contextCPU $ V.replicate (product shape) value
 
 ones :: DType a => [Int] -> IO (NDArray a)
 ones = full 1
@@ -39,7 +38,7 @@ zeros :: DType a => [Int] -> IO (NDArray a)
 zeros = full 1
 
 fromVector :: DType a => [Int] -> Vector a -> IO (NDArray a)
-fromVector shape = makeNDArray shape (Device 1 0)
+fromVector shape = makeNDArray shape contextCPU
 
 toVector :: DType a => NDArray a -> IO (Vector a)
 toVector arr = do
@@ -48,7 +47,7 @@ toVector arr = do
     VMut.unsafeWith mvec $ \p -> I.mxNDArraySyncCopyToCPU (unNDArray arr) (castPtr p) nlen
     V.unsafeFreeze mvec
 
-device :: DType a => NDArray a -> IO Device
-device (NDArray handle) = do
+context :: DType a => NDArray a -> IO Context
+context (NDArray handle) = do
     cxt <- I.mxNDArrayGetContext handle
-    return $ uncurry Device cxt
+    return $ uncurry Context cxt
--- base/c-apis/MXNet/Base/Types.hs
+++ base/c-apis/MXNet/Base/Types.hs
@@ -1,9 +1,27 @@
-{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 module MXNet.Base.Types where
 
 import Foreign.Storable (Storable)
+import Data.Proxy (Proxy(..))
+import GHC.TypeLits (Symbol, KnownSymbol)
 
-data Device = Device { _device_type :: Int, _device_id :: Int }
+data Context = Context { _device_type :: Int, _device_id :: Int }
   deriving (Eq, Show)
 
-type DType a = (Storable a, Num a, Floating a, Real a)
+class (Storable a, Show a, Num a, Floating a, Real a, KnownSymbol (DTypeName a)) => DType a where
+  type DTypeName a :: Symbol
+  typename :: a -> Proxy (DTypeName a)
+  typename a = Proxy
+
+instance DType Float where
+  type DTypeName Float = "float32"
+
+instance DType Double where
+  type DTypeName Double = "float64"
+
+contextCPU :: Context
+contextCPU = Context 1 0
+
+contextGPU0 :: Context
+contextGPU0 = Context 2 0
