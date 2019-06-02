f3ceb03afe85f80478d26ea190ccc909c9444f46
Jiasen Wu <jiasenwu@hotmail.com>
2019-06-02 22:35:59 +0200

    more on custom operation

--- base/c-apis/MXNet/Base/Symbol.hs
+++ base/c-apis/MXNet/Base/Symbol.hs
@@ -12,6 +12,9 @@ import Control.Exception.Base
 import Data.List
 import Data.Function
 import Data.Maybe
+import Control.Concurrent.MVar
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
 
 import qualified MXNet.Base.Raw as I
 
@@ -42,7 +45,7 @@ fromStorageType = fromIntegral . (subtract 1) . fromEnum
 
 data MXDType = TyNone       -- -1
              | TyFloat32    --  0
-             | TyFloat64    --  1 
+             | TyFloat64    --  1
              | TyFloat16    --  2
              | TyUInt8      --  3
              | TyInt32      --  4
@@ -63,29 +66,29 @@ class CustomOperation (Operation prop) => CustomOperationProp prop where
     prop_infer_shape :: prop -> [[Int]] -> ([[Int]], [[Int]], [[Int]])
     prop_declare_backward_dependency :: prop -> [Int] -> [Int] -> [Int] -> [Int]
     prop_infer_storage_type          :: prop -> [StorageType] -> ([StorageType], [StorageType], [StorageType])
-    prop_infer_storage_type prop in_stype = 
-        -- TODO: assert all in_stype are StorageTypeDefault
+    prop_infer_storage_type prop in_stype =
         let out_stype = replicate (length (prop_list_outputs prop)) StorageTypeDefault
             aux_stype = replicate (length (prop_list_auxiliary_states prop)) StorageTypeDefault
-        in (in_stype, out_stype, aux_stype)
-    prop_infer_storage_type_backward :: prop -> 
-                                        [StorageType] -> 
-                                        [StorageType] -> 
-                                        [StorageType] -> 
-                                        [StorageType] -> 
+            withAssert = assert (all (==StorageTypeDefault) in_stype)
+        in withAssert (in_stype, out_stype, aux_stype)
+    prop_infer_storage_type_backward :: prop ->
+                                        [StorageType] ->
+                                        [StorageType] ->
+                                        [StorageType] ->
+                                        [StorageType] ->
                                         [StorageType] ->
                                         ([StorageType], [StorageType], [StorageType], [StorageType], [StorageType])
-    prop_infer_storage_type_backward prop ograd_stype in_stype out_stype igrad_stype aux_stype = 
-        -- TODO: assert all ograd-stype are StorageTypeDefault
-        -- TODO: assert all igrad-stype are StorageTypeDefault
+    prop_infer_storage_type_backward prop ograd_stype in_stype out_stype igrad_stype aux_stype =
         let ograd_stype' = replicate (length ograd_stype) StorageTypeDefault
             in_stype'    = replicate (length in_stype)    StorageTypeDefault
             out_stype'   = replicate (length out_stype)   StorageTypeDefault
             igrad_stype' = replicate (length igrad_stype) StorageTypeDefault
             aux_stype'   = replicate (length aux_stype)   StorageTypeDefault
-        in (ograd_stype', in_stype', out_stype', igrad_stype', aux_stype')
+            withAssert   = let allDefault   = all (==StorageTypeDefault)
+                           in assert (allDefault ograd_stype && allDefault igrad_stype)
+        in withAssert (ograd_stype', in_stype', out_stype', igrad_stype', aux_stype')
     prop_infer_type :: prop -> [MXDType] -> ([MXDType],[MXDType], [MXDType])
-    prop_infer_type prop in_type = 
+    prop_infer_type prop in_type =
         let t0 = head in_type
             out_type = replicate (length (prop_list_outputs prop)) t0
             aux_type = replicate (length (prop_list_auxiliary_states prop)) t0
@@ -95,9 +98,22 @@ class CustomOperation (Operation prop) => CustomOperationProp prop where
     prop_create_operator :: prop -> [[Int]] -> [MXDType] -> IO (Operation prop)
 
 class CustomOperation op where
-    forward  :: op -> IO ()
-    backward :: op -> IO ()
+    forward  :: op -> [ReqEnum]
+             -> [I.NDArrayHandle]
+             -> [I.NDArrayHandle]
+             -> [I.NDArrayHandle]
+             -> Bool
+             -> IO ()
+    backward :: op -> [ReqEnum]
+             -> [I.NDArrayHandle]
+             -> [I.NDArrayHandle]
+             -> [I.NDArrayHandle]
+             -> [I.NDArrayHandle]
+             -> [I.NDArrayHandle]
+             -> IO ()
 
+data ReqEnum = ReqNull | ReqWrite | ReqInplace | ReqAdd
+    deriving (Bounded, Enum)
 
 registerCustomOperator :: CustomOperationProp op => (String, [(String, String)] -> IO op) -> IO ()
 registerCustomOperator (op_type, op_ctor) = do
@@ -105,32 +121,47 @@ registerCustomOperator (op_type, op_ctor) = do
     I.mxCustomOpRegister op_type ptr_creator
   where
     creator name argc keys values ret = do
+
         let argc' = fromIntegral argc
         keys_ <- peekArray argc' keys   >>= mapM peekCString
         vals_ <- peekArray argc' values >>= mapM peekCString
         prop <- op_ctor $ zip keys_ vals_
 
-        -- TODO: free them
-        ptr_args <- mapM newCString (prop_list_arguments prop) >>= newArray
-        ptr_outs <- mapM newCString (prop_list_outputs prop) >>= newArray
-        ptr_auxs <- mapM newCString (prop_list_auxiliary_states prop) >>= newArray
+        args <- mapM newCString (prop_list_arguments prop)
+        ptr_args <- newArray args
+
+        outs <- mapM newCString (prop_list_outputs prop)
+        ptr_outs <- newArray outs
+
+        auxs <- mapM newCString (prop_list_auxiliary_states prop)
+        ptr_auxs <- newArray auxs
+
         let size = 10
         ptr_callbacks <- mallocArray size
         ptr_contexts <- newArray (replicate size nullPtr)
 
-        ptr_delete_entry                <- I.mkCustomOpDelFunc (delete_entry ptr_callbacks ptr_contexts)
+        allocList <- newMVar $ [castPtr ptr_args,
+                                castPtr ptr_outs,
+                                castPtr ptr_auxs,
+                                castPtr ptr_callbacks,
+                                castPtr ptr_contexts]
+                               ++ map castPtr args
+                               ++ map castPtr outs
+                               ++ map castPtr auxs
+
+        ptr_delete_entry                <- I.mkCustomOpDelFunc (delete_entry allocList)
         ptr_list_arguments_entry        <- I.mkCustomOpListFunc (list_entry ptr_args)
         ptr_list_outputs_entry          <- I.mkCustomOpListFunc (list_entry ptr_outs)
         ptr_list_auxiliary_states_entry <- I.mkCustomOpListFunc (list_entry ptr_auxs)
-        ptr_infer_shape_entry           <- I.mkCustomOpInferShapeFunc (infer_shape_entry prop)
-        ptr_declare_backward_dependency_entry <- I.mkCustomOpBwdDepFunc (declare_backward_dependency_entry prop)
+        ptr_infer_shape_entry           <- I.mkCustomOpInferShapeFunc (infer_shape_entry allocList prop)
+        ptr_declare_backward_dependency_entry <- I.mkCustomOpBwdDepFunc (declare_backward_dependency_entry allocList prop)
         ptr_create_operator_entry       <- I.mkCustomOpCreateFunc (create_operator_entry prop)
         ptr_infer_type_entry            <- I.mkCustomOpInferTypeFunc (infer_type_entry prop)
         ptr_infer_storage_type_entry    <- I.mkCustomOpInferStorageTypeFunc (infer_storage_type_entry prop)
         ptr_infer_storage_type_backward_entry <- I.mkCustomOpBackwardInferStorageTypeFunc (infer_storage_type_backward_entry prop)
-        
+
         pokeArray ptr_callbacks [
-            castFunPtr ptr_delete_entry, 
+            castFunPtr ptr_delete_entry,
             castFunPtr ptr_list_arguments_entry,
             castFunPtr ptr_list_outputs_entry,
             castFunPtr ptr_list_auxiliary_states_entry,
@@ -144,25 +175,26 @@ registerCustomOperator (op_type, op_ctor) = do
         poke (castPtr ret) (I.MXCallbackList size ptr_callbacks ptr_contexts)
         return 0
 
-    delete_entry p1 p2 _ = do
-        free p1
-        free p2
+    delete_entry allocList _ = do
+        list <- takeMVar allocList
+        mapM_ free list
         return 0
-    
+
     list_entry cstr out_cstr _ = poke out_cstr cstr >> return 0
 
-    infer_shape_entry prop num_tensor in_out_dim_tensor in_out_shapes_tensor _ = do
+    infer_shape_entry allocList prop num_tensor in_out_dim_tensor in_out_shapes_tensor _ = do
         let num_inp = length (prop_list_arguments prop)
             num_out = length (prop_list_outputs prop)
             num_aux = length (prop_list_auxiliary_states prop)
+            num_tensor' = fromIntegral num_tensor
 
-        -- TODO: assert num_inp + num_out + num_aux == num_tensors
+        assert (num_inp + num_out + num_aux == num_tensor') (return ())
 
         -- read dimensions of inputs
         inp_dim_0 <- peekArray num_inp in_out_dim_tensor
         -- read each shape of each input
-        inp_shape_0 <- zipWithM (\i j -> do ptr <- peekElemOff in_out_shapes_tensor i 
-                                            arr <- peekArray j ptr 
+        inp_shape_0 <- zipWithM (\i j -> do ptr <- peekElemOff in_out_shapes_tensor i
+                                            arr <- peekArray j ptr
                                             return $ map fromIntegral arr)
                                 [0..] (map fromIntegral inp_dim_0)
 
@@ -170,30 +202,34 @@ registerCustomOperator (op_type, op_ctor) = do
             all_shape = inp_shape ++ out_shape ++ aux_shape
             all_shape_sizes = map length all_shape
 
-        -- TODO: assert length all_shape == num_tensors
+        assert (length all_shape == num_tensor') (return ())
+
         pokeArray in_out_dim_tensor (map fromIntegral all_shape_sizes)
-        -- TODO: free ptr
         -- no need to allocate new dimensions of inputs/outputs/auxiliaries
         -- only need to allocate new shapes for each
         ptr_0 <- newArray (map fromIntegral (concat all_shape) :: [CInt])
+        modifyMVar_ allocList (return . (castPtr ptr_0 :))
+
         let offsets = scanl (+) 0 all_shape_sizes
             ptrs = map (plusPtr ptr_0) offsets
         pokeArray in_out_shapes_tensor ptrs
         return 0
 
-    declare_backward_dependency_entry prop grad_out data_in data_out out_num_dep out_deps _ = do
+    declare_backward_dependency_entry allocList prop grad_out data_in data_out out_num_dep out_deps _ = do
         let num_inp = length (prop_list_arguments prop)
             num_out = length (prop_list_outputs   prop)
         grad_out_inds <- peekArray num_out grad_out
         data_in_inds  <- peekArray num_inp data_in
         data_out_inds <- peekArray num_out data_out
-        let deps = prop_declare_backward_dependency prop 
+        let deps = prop_declare_backward_dependency prop
                         (map fromIntegral grad_out_inds)
                         (map fromIntegral data_in_inds)
                         (map fromIntegral data_out_inds)
         poke out_num_dep (fromIntegral $ length deps)
         -- TODO: free ptr
         ptr_deps <- newArray (map fromIntegral deps)
+        modifyMVar_ allocList (return . (castPtr ptr_deps :))
+
         poke out_deps ptr_deps
         return 0
 
@@ -202,7 +238,8 @@ registerCustomOperator (op_type, op_ctor) = do
             num_out = length (prop_list_outputs prop)
             num_aux = length (prop_list_auxiliary_states prop)
             num_tensor' = fromIntegral num_tensor
-        -- TODO: assert num_tensor == num_inp + num_out + num_aux
+        assert (num_inp + num_out + num_aux == num_tensor') (return ())
+
         inp_types0 <- map toMXDType <$> peekArray num_inp tensor_types
         let (inp_types, out_types, aux_types) = prop_infer_type prop inp_types0
             all_types = map fromMXDType $ inp_types ++ out_types ++ aux_types
@@ -214,12 +251,13 @@ registerCustomOperator (op_type, op_ctor) = do
             num_out = length (prop_list_outputs prop)
             num_aux = length (prop_list_auxiliary_states prop)
             num_tensor' = fromIntegral num_tensor
-        -- TODO: assert num_tensor == num_inp + num_out + num_aux
+        assert (num_inp + num_out + num_aux == num_tensor') (return ())
+
         inp_stypes0 <- map toStorageType <$> peekArray num_inp tensor_stypes
         let (inp_stypes, out_stypes, aux_stypes) = prop_infer_storage_type prop inp_stypes0
             all_stypes = map fromStorageType $ inp_stypes ++ out_stypes ++ aux_stypes
         pokeArray tensor_stypes all_stypes
-        return 0 
+        return 0
 
     infer_storage_type_backward_entry prop num_tensor tensor_stypes tags _ = do
         let num_inp = length (prop_list_arguments prop)
@@ -230,10 +268,11 @@ registerCustomOperator (op_type, op_ctor) = do
         tensor_types' <- peekArray num_tensor' tensor_stypes
         let tensors = groupBy ((==) `on` fst) (zip tags' tensor_types')
             tensors_table = map (\t -> (fst (head t), map (toStorageType . snd) t)) tensors
-        -- TODO: assert tensors of length 5
-            [ograd0, input0, output0, igrad0, aux0] = map (fromJust . flip lookup tensors_table) [3, 0, 1, 2, 4] 
+            [ograd0, input0, output0, igrad0, aux0] = map (fromJust . flip lookup tensors_table) [3, 0, 1, 2, 4]
             (ograd, input, output, igrad, aux) = prop_infer_storage_type_backward prop ograd0 input0 output0 igrad0 aux0
             all_stypes = map fromStorageType $ ograd ++ input ++ output ++ igrad ++ aux
+
+        assert (length tensors == 5) (return ())
         pokeArray tensor_stypes all_stypes
         return 0
 
@@ -247,10 +286,12 @@ registerCustomOperator (op_type, op_ctor) = do
         op <- prop_create_operator prop shapes dtypes
 
         let size = 3
+        -- TODO: free ptrs
         ptr_callbacks <- mallocArray size
         ptr_contexts  <- newArray (replicate size nullPtr)
+        allocList <- newMVar $ [castPtr ptr_callbacks, castPtr ptr_contexts]
 
-        ptr_delete_entry        <- I.mkCustomFunctionDelFunc (func_delete_entry ptr_callbacks)
+        ptr_delete_entry        <- I.mkCustomFunctionDelFunc (delete_entry allocList)
         ptr_func_forward_entry  <- I.mkCustomFunctionBwdFunc (func_forward_entry  op)
         ptr_func_backward_entry <- I.mkCustomFunctionBwdFunc (func_backward_entry op)
         pokeArray ptr_callbacks [
@@ -261,10 +302,49 @@ registerCustomOperator (op_type, op_ctor) = do
         poke ret (I.MXCallbackList size ptr_callbacks ptr_contexts)
         return 0
 
-    func_forward_entry op num_ndarray ndarraies tags reqs is_train _ = return 0
+    func_forward_entry op num_ndarray ndarrays tags reqs is_train _ = do
+        let num_ndarray_ = fromIntegral num_ndarray
+        ndarrays <- peekArray num_ndarray_ ndarrays >>= mapM I.newNDArrayHandle
+        tags     <- peekArray num_ndarray_ tags
 
-    func_backward_entry op num_ndarray ndarraies tags reqs is_train _ = return 0
+        let tensors = V.map reverse $ V.create $ do
+                        vec <- VM.replicate 5 []
+                        forM (zip tags ndarrays) $ \(ind, hdl) -> do
+                            let ind_ = fromIntegral ind
+                            lst <- VM.read vec ind_
+                            VM.write vec ind_ (hdl : lst)
+                        return vec
 
-    func_delete_entry p1 _ = do
-        free p1
+        let in_data  = tensors ! 0
+            out_data = tensors ! 1
+            aux      = tensors ! 4
+
+        reqs <- map (toEnum . fromIntegral) <$> peekArray (length out_data) reqs
+        forward op reqs in_data out_data aux (is_train == 1)
+
         return 0
+
+    func_backward_entry op num_ndarray ndarrays tags reqs is_train _ = do
+        let num_ndarray_ = fromIntegral num_ndarray
+        ndarrays <- peekArray num_ndarray_ ndarrays >>= mapM I.newNDArrayHandle
+        tags     <- peekArray num_ndarray_ tags
+
+        let tensors = V.map reverse $ V.create $ do
+                        vec <- VM.replicate 5 []
+                        forM (zip tags ndarrays) $ \(ind, hdl) -> do
+                            let ind_ = fromIntegral ind
+                            lst <- VM.read vec ind_
+                            VM.write vec ind_ (hdl : lst)
+                        return vec
+
+        let in_data  = tensors ! 0
+            out_data = tensors ! 1
+            in_grad  = tensors ! 2
+            out_grad = tensors ! 3
+            aux      = tensors ! 4
+
+        reqs <- map (toEnum . fromIntegral) <$> peekArray (length out_data) reqs
+        backward op reqs in_data out_data in_grad out_grad aux
+        return 0
+
+    (!) = (V.!)
