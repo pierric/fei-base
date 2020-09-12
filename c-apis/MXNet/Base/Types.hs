60f93145d4f8edfe041a04809e5703c17e6563fc
Jiasen Wu <jiasenwu@hotmail.com>
2020-09-12 21:14:39 +0200

    split to DType and FloatDType

--- c-apis/MXNet/Base/Types.hs
+++ c-apis/MXNet/Base/Types.hs
@@ -16,25 +16,32 @@ data Context = Context
 class (Storable a,
        Unbox a,
        Show a,
+       Eq a,
        Num a,
-       Floating a,
-       Real a,
-       RealFloat a,
-       RealFrac a,
        KnownSymbol (DTypeName a)) => DType a where
-  type DTypeName a :: Symbol
-  typename :: a -> Proxy (DTypeName a)
-  typename a = Proxy
+    type DTypeName a :: Symbol
+    typename :: a -> Proxy (DTypeName a)
+    typename a = Proxy
 
-  flag :: a -> Int
+    flag :: a -> Int
+
+
+class (RealFloat a, DType a) => FloatDType a
 
 instance DType Float where
-  type DTypeName Float = "float32"
-  flag _ = 0    -- mshadow::kFloat32
+    type DTypeName Float = "float32"
+    flag _ = 0    -- mshadow::kFloat32
 
 instance DType Double where
-  type DTypeName Double = "float64"
-  flag _ = 1    -- mshadow::kFloat64
+    type DTypeName Double = "float64"
+    flag _ = 1    -- mshadow::kFloat64
+
+instance FloatDType Float
+instance FloatDType Double
+
+instance DType Word8 where
+    type DTypeName Word8 = "uint8"
+    flag _ = 3    -- mshadow::kUint8
 
 contextCPU :: Context
 contextCPU = Context 1 0
