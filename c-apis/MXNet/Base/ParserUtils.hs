322ac91143ad81a155663dbcda0bcf08eb140e29
Jiasen Wu <jiasenwu@hotmail.com>
2020-10-23 20:13:25 +0200

    minor

--- c-apis/MXNet/Base/ParserUtils.hs
+++ c-apis/MXNet/Base/ParserUtils.hs
@@ -13,5 +13,10 @@ import           RIO
 parseR :: HasCallStack => P.Parser a -> Text -> a
 parseR c t = (parseOnly (c <* endOfInput) t) ^?! _Right
 
-list  f = char '[' *> sepBy f (char ',') <* char ']'
+list  f = char '[' *> commaSep f <* char ']'
 tuple f = char '(' *> liftA2 (,) f (char ',' *> f) <* char ')'
+
+-- TODO give type level int, build a tuple of that size
+many f = char '(' *> commaSep f <* char ')'
+
+commaSep p  = p `sepBy` (char ',')
