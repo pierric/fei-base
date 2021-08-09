702309c4b43c16e07efa9ad9f16289fb06db1efd
Jiasen Wu <jiasenwu@hotmail.com>
2021-08-09 06:07:37 +0200

    Update operators  (#26)
    * hide Functional module when importing the base
    
    * rename ones and zeros in NDArray module
    
    * add broadcastLikeAxis and scatter, update softmaxCE

--- c-apis/MXNet/Base/Tensor.hs
+++ c-apis/MXNet/Base/Tensor.hs
@@ -1,7 +1,6 @@
 module MXNet.Base.Tensor(
     module MXNet.Base.Tensor.Class,
-    module MXNet.Base.Tensor.Functional
-    ) where
+) where
 
 import           MXNet.Base.Tensor.Class
 import           MXNet.Base.Tensor.Functional
