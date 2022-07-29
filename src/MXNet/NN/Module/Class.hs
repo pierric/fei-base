a0dc7f4ca5727266c7221a83a2a504f83c657371
Jiasen Wu <jiasenwu@hotmail.com>
2022-07-29 12:06:13 +0200

    improved Module

--- src/MXNet/NN/Module/Class.hs
+++ src/MXNet/NN/Module/Class.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE ImplicitParams #-}
 module MXNet.NN.Module.Class where
 
+import qualified Data.HashMap.Strict         as M
+import           Data.Hashable
 import           RIO
 import qualified RIO.NonEmpty                as NE
 import qualified RIO.NonEmpty.Partial        as NE (fromList)
 import qualified RIO.Text                    as T
+import           Type.Reflection             (typeOf, eqTypeRep, (:~~:)(..))
 
 import           MXNet.Base.AutoGrad
 import qualified MXNet.Base.Operators.Tensor as O
@@ -12,22 +15,60 @@ import           MXNet.Base.Tensor
 import           MXNet.Base.Types
 import           MXNet.NN.Initializer
 
-data Parameter t where
-    Parameter :: DType t => Text -> NDArray t -> Parameter t
+data ParameterPath where
+    (:>) :: (Enum e, Eq e, Typeable e) => NonEmpty Text -> e -> ParameterPath
+
+infix 4 :>
+
+instance Eq ParameterPath where
+    (t1 :> e1) == (t2 :> e2)
+        | t1 /= t2 = False
+        | otherwise = case eqTypeRep (typeOf e1) (typeOf e2) of
+                        Just HRefl -> e1 == e2
+                        Nothing   -> False
+
+instance Hashable ParameterPath where
+    hashWithSalt s (t :> e) = hashUsing fromEnum (hashWithSalt s t) e
+
+type InitializerTable u = HashMap ParameterPath (Initializer u)
+
+type ParameterTable   u = HashMap ParameterPath (NDArray u)
 
 class Module m where
-    type ModuleDType m
-    type ModuleArgs  m
-    type ModuleParamEnums   m
-    type ModuleParamTensors m
+    type ModuleDType  m
+    type ModuleArgs   m
+    type ModuleInput  m
+    type ModuleOutput m
     init        :: (HasCallStack, ?device :: Context)
                 => NonEmpty Text
                 -> ModuleArgs m
-                -> Map (ModuleParamEnums m) (Initializer (ModuleDType m))
+                -> InitializerTable (ModuleDType m)
                 -> IO m
     forward     :: (HasCallStack, ?device :: Context)
-                => m -> NDArray (ModuleDType m) -> IO (NDArray (ModuleDType m))
-    parameters  :: HasCallStack => m -> IO [Parameter (ModuleDType m)]
+                => m -> ModuleInput m -> IO (ModuleOutput m)
+    parameters  :: HasCallStack => m -> IO (ParameterTable (ModuleDType m))
+
+
+data GenericModule e u = GenericModule {
+    _gmodule_scope       :: NonEmpty Text,
+    _gmodule_initializer :: InitializerTable u,
+    _gmodule_parameters  :: IORef (Maybe (HashMap e (NDArray u)))
+}
+
+instance (Enum e, DType u) => Module (GenericModule e u) where
+    type ModuleDType (GenericModule e u) = u
+    type ModuleArgs  (GenericModule e u) = ()
+
+    -- make a non-initilized generic module
+    init scope args user_init = do
+        params <- newIORef Nothing
+        return $ GenericModule scope user_init params
+
+    parameters generic = do
+        p <- getOrInitParams generic (error "module not initialized yet.")
+        return $ M.mapKeys (\e -> _gmodule_scope generic :> fromEnum e) p
+
+    forward _ _ = error "generic module doesn't have forward"
 
 
 scopedName :: NonEmpty Text -> Text
@@ -35,15 +76,6 @@ scopedName scope = foldl' T.append "" $ NE.intersperse scopeSplitter $ NE.revers
     where
     scopeSplitter = "."
 
-data GenericModule m = GenericModule {
-    _gmodule_name        :: Text,
-    _gmodule_initializer :: Map (ModuleParamEnums m)  (Initializer (ModuleDType m)),
-    _gmodule_tensors     :: IORef (Maybe (ModuleParamTensors m))
-}
-
-initGenericModule scope initializer = do
-    params <- newIORef Nothing
-    return $ GenericModule (scopedName scope) initializer params
 
 getOrInitParams (GenericModule _ initializer ref) make = do
     mparams <- readIORef ref
@@ -52,4 +84,3 @@ getOrInitParams (GenericModule _ initializer ref) make = do
                    return p
     maybe make' return mparams
 
-getParams genric = getOrInitParams genric (error "module not initialized yet.")
